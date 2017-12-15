3d34259f58238113b71f27f520d8157a4eb43f7b
Tarik <4taras4@gmail.com>
2017-12-15 22:14:06 +0200

    v 1.6

--- CountryPicker/Classes/CountryPicker.swift
+++ CountryPicker/Classes/CountryPicker.swift
@@ -230,7 +230,7 @@ open class CountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSo
             let selectedRowFrame: CGRect = self.bounds.insetBy(dx: 0, dy: (self.frame.height - rowHeight) / 2.0)
             let userTappedOnSelectedRow = selectedRowFrame.contains(tapRecognizer.location(in: self))
             if (userTappedOnSelectedRow) {
-                let selectedRow = self.pickerView(self, didSelectRow: 0, inComponent: 0)
+                _ = self.pickerView(self, didSelectRow: 0, inComponent: 0)
             }
         }
     }
