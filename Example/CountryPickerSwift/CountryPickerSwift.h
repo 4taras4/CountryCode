3b815d6ff7ae5c8cb58461aee8d375b4f9f91a78
Tarik <4taras4@gmail.com>
2018-11-30 22:55:25 +0200

    Added cart

---
+++ Example/CountryPickerSwift/CountryPickerSwift.h
@@ -0,0 +1,19 @@
+//
+//  CountryPickerSwift.h
+//  CountryPickerSwift
+//
+//  Created by Taras Markevych on 11/30/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CountryPickerSwift.
+FOUNDATION_EXPORT double CountryPickerSwiftVersionNumber;
+
+//! Project version string for CountryPickerSwift.
+FOUNDATION_EXPORT const unsigned char CountryPickerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountryPickerSwift/PublicHeader.h>
+
+
