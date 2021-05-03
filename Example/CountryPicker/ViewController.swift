4b8db23fb19787411b8a0d02887e1b82158e9802
Chris So <chris.so@crypto.com>
2021-05-03 17:39:05 +0800

    🗑️ cleanup example

--- Example/CountryPicker/ViewController.swift
+++ Example/CountryPicker/ViewController.swift
@@ -6,31 +6,28 @@
 //  Copyright (c) 2016 4taras4. All rights reserved.
 //
 
-import UIKit
 import CountryPicker
+import UIKit
 
-class ViewController: UIViewController, CountryPickerDelegate  {
-   
-    @IBOutlet weak var picker: CountryPicker!
-    @IBOutlet weak var code: UILabel!
+class ViewController: UIViewController, CountryPickerDelegate {
+    @IBOutlet var picker: CountryPicker!
+    @IBOutlet var code: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //get corrent country
+        // get corrent country
         let locale = Locale.current
         let code = (locale as NSLocale).object(forKey: NSLocale.Key.countryCode) as! String?
-        //init Picker
+        // init Picker
         picker.countryPickerDelegate = self
         picker.showPhoneNumbers = true
 //        let theme = CountryViewTheme(countryCodeTextColor: .white, countryNameTextColor: .white, rowBackgroundColor: .black, showFlagsBorder: true) //optional
 //        picker.theme = theme //optional
         picker.setCountry(code!)
     }
-  
+
     // MARK: - CountryPhoneCodePicker Delegate
+
     public func countryPhoneCodePicker(_ picker: CountryPicker, didSelectCountryWithName name: String, countryCode: String, phoneCode: String, flag: UIImage) {
         code.text = phoneCode
     }
-
 }
-
-
