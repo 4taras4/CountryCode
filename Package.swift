00cddcfa1afbb2f105465376abdfed047e1800d5
4taras4 <4taras4@gmail.com>
2022-07-14 23:03:13 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "CountryPicker",
+    name: "CountryCode",
     platforms: [
       .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CountryPicker",
-            targets: ["CountryPicker"]),
+            name: "CountryCode",
+            targets: ["CountryCode"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,13 +22,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CountryPicker",
+            name: "CountryCode",
             dependencies: [],
             exclude:[],
             resources: [
                 .process("CountryPicker")]),
         .testTarget(
             name: "CountryCodeTests",
-            dependencies: ["CountryPicker"]),
+            dependencies: ["CountryCode"]),
     ]
 )
